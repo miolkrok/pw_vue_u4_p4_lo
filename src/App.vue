f0e68f9d4c1300e647399ef55e23384cb0e3a731
luis <luis@gmail.com>
2023-07-29 10:53:50 -0500

    TALLER 35

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <Estudiante/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Estudiante from './modules/estudiante/pages/Estudiante.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Estudiante
   }
 }
 </script>
